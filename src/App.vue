bb50cb8df2c50c87a90d795881c68552395eea62
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-03 19:33:00 +0600

    alert component use

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup>
-import { ref } from "vue";
-const time="mornig";
-
-const Countries=ref([
-  {name:'Bangladesh', capital:'Dhaka'},
-  {name:'Bangladesh', capital:'Dhaka'},
-  {name:'Bangladesh', capital:'Dhaka'},
-  {name:'Bangladesh', capital:'Dhaka'},
-]);
-
-
+import AlertComponents from './components/AlertComponents.vue';
 
 </script>
 
@@ -18,23 +8,13 @@ const Countries=ref([
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-     <h1 v-if="time=='morning'">Good Morning </h1>
-     <h1 v-else-if="time=='afternoon'">Good Afternoon </h1>
-     <h1 v-else>Good Evening</h1>
-
-
-     <h1>Countries With Capitals :</h1>
-     <ul>
-      <li v-for="country in Countries" :key="country.name">
-        {{ country.name }} - {{ country.capital }}
-      </li>
-     </ul>
-     <ul>
-      <li v-for="country, index in Countries">
-       {{ index+1 }} {{ country.name }} - {{ country.capital }}
-      </li>
-     </ul>
     
+    <AlertComponents type="error"  msg="hello world"/><br>
+
+    <alert-components type="success" msg="success" /><br>
+    <alert-components type="warning"/>
+    <alert-components />
+      
     </div>
   </header>
 
---
+++ src/components/AlertComponents.vue
@@ -0,0 +1,36 @@
+<script setup>
+// const type="error";
+// const msg="this is an message !";
+// defineProps(['type','msg']);
+   
+ defineProps({
+   type:{
+    type:String,
+    default:'info',
+   },
+   msg:{
+    type:String,
+    default:"this is alert message ",
+   }
+ });
+
+const GetColors=(type)=>{
+  const colors={
+    'info':'bg-blue-100 text-blue-800',
+    'success':'bg-green-100 text-green-800',
+    'error':'bg-red-100 text-red-800',
+    'warning':'bg-yellow-100 text-yellow-800',
+  }
+  return colors[type] || colors['info'];
+}
+</script>
+
+<template>
+<div class="p-4 mb-4 text-sm  rounded-lg " :class="GetColors(type)" role="alert">
+  <span class="font-medium">{{ msg }}</span>
+</div>
+</template>
+
+<style scoped>
+
+</style>
