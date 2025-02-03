f16fbe25c85723122b8b97e048d311cbdb6b573f
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-03 14:36:19 +0600

    v-if logic apply

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <script setup>
-    const message="Wellcome Get Reader to master <em>Vue-Js -3</em>";
-    const number =16;
-    const Animal="Tiger";
-    const seacrUrl="https://www.pexels.com/search/";
+import { ref } from 'vue';
 
+const location="Dhaka";
+const weather=ref("s")
 </script>
 
 
 <template>
- <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-      <h1 class="text-4xl" v-html="message"></h1>
-      <h2 class="text-xl mt-10 text-gary-700"><span> {{ number }}/4 = {{ number / 4 }} </span></h2>
-      <h2 class="text-xl mt-10 text-gary-700"><span> {{ number }} is  = {{ number %2==0?"Even number":"Odd Number" }} </span></h2>
-      <h2 class="text-xl mt-10 text-gary-700">
-        <a target="_blank" :href="seacrUrl+Animal.toLowerCase()"> Search For {{ Animal  }}</a>
-      
-      </h2>
-      <h2 class="text-xl mt-10 text-gary-700">
-        <a target="_blank" :href="`https://www.pexels.com/search/${Animal}`"> Search For {{ Animal  }}</a>
-      
-      </h2>
- </div>
+
+<div class="row">
+  <div class="card-header">
+   <h1>Weather Map</h1>
+  </div>
+</div>
+<h1 class="text-3xl ">{{ location }}</h1>
+<h2 text-2xl v-if="weather=='s'" >Weather :Sunny</h2>
+<h2 text-2xl v-if="weather=='c'" >Weather :Cloudly</h2>
+<h2 text-2xl v-if="weather=='r'" >Weather :RainDay</h2>
+<h2 text-2xl v-if="weather=='t'" >Weather :ThunderStrom</h2>
+<img v-if="weather=='s'" src="https://cdn.pixabay.com/photo/2016/03/18/15/07/cloudiness-1265205_1280.png" alt="" class="images">
+<img v-if="weather=='c'" src="https://www.svgrepo.com/show/502423/weather.svg" alt="" class="images">
+<img text-2xl v-if="weather=='r'" src="https://www.svgrepo.com/show/279148/snowing-forecast.svg" alt="" class="images">
+<img text-2xl v-if="weather=='t'" src="https://www.svgrepo.com/show/279158/sunrise.svg" alt="" class="images">
+
+
+<input type="text" placeholder="Weather Status" class="text-center p-5" v-model="weather">
 </template>
 
 
 <style scoped>
-
+.images{ 
+  width: 90px;
+  height: 90px;
+  margin: 10px;
+  padding: 5px;
+}
 </style>
