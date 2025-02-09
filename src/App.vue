a34145c99516bd1e569e4a49ee1ff65835d6b737
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-09 11:03:30 +0600

    clean vue-js file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <script setup>
-import { reactive } from 'vue';
-
-
-const myFormData=reactive({
-    user:"",
-    password:"",
-})
-function formdata(){
-    console.log(myFormData);
-}
+import CountryList from './components/CountryList.vue';
 </script>
 
 <template>
-  <form @submit.prevent="formdata" >
-    <label >User Name =>{{ myFormData.user }}</label><br>
-    <input v-model="myFormData.user" type="text" placeholder="User Name"><br>
-
-    
-    <label >Password=>{{ myFormData.password }}</label><br>
-    <input v-model="myFormData.password" type="possword" placeholder="Password"><br>
-
-    <button type="submit">submit</button>
-
-  </form>
+<CountryList />
 </template>
 
 <style scoped>
