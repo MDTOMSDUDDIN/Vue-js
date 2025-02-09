1e1018784eeb47722495584ed1ffa29146f8de39
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-09 20:38:00 +0600

    style binding

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <script setup>
-const bgOrange=" bg-orange-400";
-const bgindigo=" bg-indigo-400";
-const bggreen=" bg-green-400";
+const bgOrange='background-color:red';
+const bgindigo='background-color:green';
+const bggreen='background-color:orange';
+
+const bgRed='background-color:red';
+const bgDarkGreen='background-color:green';
+
 
 const weekday=true;
 
 const colors={
-    bgColor:"bg-teal-100",
-    borderColor:"border-teal-500",
-    textColor:"text-teal-900",
+    backgroundColor:"info",
+    borderColor:"info",
+   color:'white',
 }
-
-const messageType='warning';
+const messageType='success';
 
 if('error'==messageType){
-    colors.bgColor='bg-red-100',
-    colors.borderColor='border-red-500',
-    colors.textColor='text-red-900'
+    colors.backgroundColor='red',
+    colors.borderColor='red',
+    colors.color='white'
 }
 else if('success'==messageType){
-    colors.bgColor='bg-green-100',
-    colors.borderColor='border-green-500',
-    colors.textColor='text-green-900'
+    colors.backgroundColor='green',
+    colors.borderColor='darkgreen',
+    colors.color='white'
 }
 else if('warning'==messageType){
-    colors.bgColor='bg-orange-100',
-    colors.borderColor='green-orange-500',
-    colors.textColor='text-orange-900'
+    colors.backgroundColor='orange',
+    colors.borderColor='orange',
+    colors.color='white'
 }
 
 </script>
@@ -36,19 +39,19 @@ else if('warning'==messageType){
         <h4 class="text-center py-12 text-3xl">Class & Style binding</h4>
 
         <section class="flex space-x-10">
-        <div class="w-1/3 h-16" :class="bgOrange" ></div>
-        <div class="w-1/3 h-16" :class="bgindigo" ></div>
-        <div class="w-1/3 h-16" :class="bggreen"></div>
+        <div class="w-1/3 h-16" :style="bgOrange" ></div>
+        <div class="w-1/3 h-16" :style="bgindigo"></div>
+        <div class="w-1/3 h-16" :style="bggreen"></div>
         </section>
     
         <section class="mt-10 ">
-            <div class=" h-16 flex items-center justify-center text-white" :class="[weekday?'bg-red-600':'bg-green-600', weekday==true?'text-2xl':'text-4xl']">
+            <div class=" h-16 flex items-center justify-center text-white" :style="[weekday?bgRed:bgDarkGreen, weekday==true?'text-2xl':'text-4xl']">
                 Its is a Weekday !
             </div>
         </section>
 
         <section class="mt-10 ">
-            <div class=" border-t-4 rounded-b py-3 shadow-md" :class="[colors.bgColor,colors.borderColor,colors.textColor]">
+            <div class=" border-t-4 rounded-b py-3 shadow-md" :style="colors">
                 <p class="font-blod">Our Privicy Policy Has Changed</p>
                 <p class="text-sm">Make sure you Know how these change Effet</p>
             </div>
