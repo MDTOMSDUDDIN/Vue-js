2a295a3a8e9ab4998df849514b4dd299c1c50d64
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-10 22:04:32 +0600

    using dynamic  create an accordion

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,58 @@
 <script setup>
-import { reactive } from 'vue';
-
+import { reactive, ref } from 'vue';
 const accordins=reactive([
-    false,false,false,false
+ {
+    heading:"Whats is teams ?",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deleniti voluptatibus cupiditate quos modi quaerat, omnis aperiam cumque doloremque distinctio exercitationem architecto beatae. Inventore quis suscipit in pariatur ab et!",
+    isOpen:true
+ },
+ 
+ {
+    heading:"When using to Accodion Component",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deleniti voluptatibus cupiditate quos modi quaerat, omnis aperiam cumque doloremque distinctio exercitationem architecto beatae. Inventore quis suscipit in pariatur ab et!",
+    isOpen:false
+ },
+ {
+    heading:"How can its defined ?",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deleniti voluptatibus cupiditate quos modi quaerat, omnis aperiam cumque doloremque distinctio exercitationem architecto beatae. Inventore quis suscipit in pariatur ab et!",
+    isOpen:false
+ },
+ {
+    heading:"to do nothing ?",
+    content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci deleniti voluptatibus cupiditate quos modi quaerat, omnis aperiam cumque doloremque distinctio exercitationem architecto beatae. Inventore quis suscipit in pariatur ab et!",
+    isOpen:false
+ }
+
 ]) ;
 
-function toggle(index){
-    accordins[index]= ! accordins[index];
-}
 </script>
  
 <template>
 <section class="container mx-auto flex item-center flex-col bg-slate-400 ">
     <h1 class="text-center text-5xl py-10">Event and Reactivity in vue js </h1>
+
     <div class="p-10 bg-gridient-to-br from-indifo-100 to-indigo-300 grid place-items-center ">
      <div class="w-6/12 mx-auto rounded border">
+        <div class=" border-blue-100 text-center mb-2 shadow-lg bg-yellow-50 ">
+            <h1 class=" font-medium text-gray-800  text-4xl">Servel  windows stacks on each others</h1>
+            <p class="text-4xl">The accordin and control this Elements </p>
+        </div>
      <div class="bg-white p-18 shadow-sm">
-        <h1 class=" font-medium text-gray-800  text-4xl">Servel  windows stacks on each others</h1>
-        <p class="text-4xl">The accordin and control this Elements </p>
-        <div class="h-1 w-full mx-auto border-b my-5"></div>
         
-        <div  class="transti hover:bg-indigo-50 ">
+        
+        <div class="h-1 w-full mx-auto border-b my-5"></div>
+  
+        <div  class="transti hover:bg-indigo-50" v-for="accordin in accordins">
             <!-- header -->
-            <div @click="toggle(0)" class=" accordion-header cursor-pointer transition flex space-x-5 px-5 items-center text-3xl pb-6 ">
+            <div @click="accordin.isOpen=! accordin.isOpen" class=" accordion-header cursor-pointer transition flex space-x-5 px-5 items-center text-3xl pb-6 ">
                 <i class="fa fa-plus"></i>
-                <h3>What is Teams</h3>
+                <h3>{{ accordin.heading }}</h3>
             </div>
             <!-- content -->
-            <div class=" px-5 pt-0 text-left pb-5" v-show="accordins[0]">
-            <div class=" leading-5 font-light pl-9 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis placeat delectus fugit provident debitis voluptatibus quae non beatae natus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, labore!
+            <div class=" px-5 pt-0 text-left pb-5">
+            <div class=" leading-5 font-light pl-9" v-show="accordin.isOpen">
+                {{ accordin.content }}
             </div>
-         </div>
-        </div>
-
-        <div class="transti hover:bg-indigo-50 ">
-            <!-- header -->
-            <div @click="toggle(1)" class=" accordion-header cursor-pointer transition flex space-x-5 px-5 items-center text-3xl pb-6  ">
-                <i class="fa fa-plus"></i>
-                <h3>accordion-header part 2</h3>
-            </div>
-            <!-- content -->
-            <div class=" px-5 pt-0 text-left pb-5" v-show="accordins[1]">
-            <p class=" leading-5 font-light pl-9 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis placeat delectus fugit provident debitis voluptatibus quae non beatae natus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, labore!
-            </p>
-         </div>
-        </div>
-
-        <div class="transti hover:bg-indigo-50 ">
-            <!-- header -->
-            <div @click="toggle(2)" class=" accordion-header cursor-pointer transition flex space-x-5 px-5 items-center text-3xl pb-6 ">
-                <i class="fa fa-plus"></i>
-                <h3>accordion-header part 3</h3>
-            </div>
-            <!-- content -->
-            <div class=" px-5 pt-0 text-left pb-5 " v-show="accordins[2]">
-            <p class=" leading-5 font-light pl-9 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis placeat delectus fugit provident debitis voluptatibus quae non beatae natus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, labore!
-            </p>
-         </div>
-        </div>
-
-        <div class="transti hover:bg-indigo-50 ">
-            <!-- header -->
-            <div  @click="toggle(3)" class=" accordion-header cursor-pointer transition flex space-x-5 px-5 items-center text-3xl pb-6 ">
-                <i class="fa fa-plus"></i>
-                <h3>accordion-header part 4</h3>
-            </div>
-            <!-- content -->
-            <div class=" px-5 pt-0 text-left pb-5 " v-show="accordins[3]">
-            <p class=" leading-5 font-light pl-9 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis placeat delectus fugit provident debitis voluptatibus quae non beatae natus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, labore!
-            </p>
          </div>
         </div>
 
