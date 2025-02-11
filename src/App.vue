4761c81b01e5c983a284a1332350efc9796f0111
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-11 20:12:03 +0600

    carosuel onMounted hooks flickity package

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,47 @@
 <script setup>
-import { onBeforeMount, onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
 
-const status=ref([])
-const apiResponse=ref(null)
-onBeforeMount(()=>{
-   console.log("Before Mount ")
-   status.value.push("Before Mount")
+ const items=ref([
+    'https://images.unsplash.com/photo-1738330094168-6e9728731ce8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1736177843409-73956dc1c810?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1738566491473-713a2a865c0f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+ ]);
+ 
+ let carosuel =null ;
+ 
+ const numItems=ref('https://plus.unsplash.com/premium_photo-1669865742415-596c9a489d77?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0Mnx8fGVufDB8fHx8fA%3D%3D')
 
-   fetch('https://jsonplaceholder.typicode.com/todos/2')
-   .then(Response=>Response.json())
-   .then(json=>{
-      apiResponse.value=json
-   })
-})
+ function addNewItem(){
+    items.value.push(numItems.value)
+    carosuel.destroy()
+    nextTick(function(){
+        carosuel = new Flickity( '#carosuel', {})
+    })
+ }
+ 
+ onMounted(()=>{
+   carosuel = new Flickity( '#carosuel', {})
+ })
 
-onMounted(()=>{
-   console.log(" Mounted ")
-   status.value.push("Mounted")
-})
 </script>
  
 <template>
-<section class=" container mx-auto  items-center flex flex-col">
-   <h1 class=" text-4xl text-center m-3 p-3">Life cycle Hooks</h1> 
-   <hr class="border-t border-red-500  w-full">
-  <div class=" flex justify-between mt-20">
-   <div class="w-1/2">
-      <img  src="https://vuejs.org/assets/lifecycle.MuZLBFAS.png" alt="">
-   </div>
-   <div class="text-2xl w-1/2">
-      {{ status }}
-   </div>
-   <p>{{ apiResponse }}</p>
-  </div>
+    <div class="mb-10 text-4xl text-center">Life Cycle Hooks onMounted</div>
+    <div class="mb-10 text-center space-x-6">
+        <input type="text" v-model="numItems">
+        <button @click="addNewItem()" class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">submit </button>
+    </div>
+    
+    <div class="mx-auto item " id="carosuel">
+        <div :style="`background-image:url(${item})`" class="item" v-for="(item,index) in items" :key="item">{{ index+1 }}</div>
+    </div>
 
-
- 
-</section>
 </template>
  
 <style scoped>
- 
-</style> 
+ .item{
+    width: 900px;
+    height: 400px;
+
+ }
+</style>
