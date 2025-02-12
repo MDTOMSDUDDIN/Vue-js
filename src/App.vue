44d13d5375dfbfe2d3b19fb2aa32292f9d8a0dfe
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-12 21:14:02 +0600

    Simple quiz project vue js

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
- 
-  const multibox=ref([])
+ const saarsCountries=reactive([
+  {name:'bangladesh', capital:'Dhaka'},
+   {name:'Canada', capital:'Ottawa'},
+  {name:'Brazil', capital:'Brasília'},
+  {name:'Japan', capital:'Tokyo'},
+  {name:'Australia', capital:'Canberra'},
+  {name:'South Africa', capital:'Pretoria'}
+ ])
+ const capitals=["Dhaka","Ottawa","Brasília","Tokyo","Canberra","Pretoria"]
+
+ function getRandomCapitials(){
+  // return capitals.sort(()=>Math.random() -0.5)
+  return capitals
+ }
+
+ function getscore(){
+  let Score=0;
+  saarsCountries.forEach(country=>{
+    if(country.answer===country.capital){
+      Score++
+    }
+  })
+  return Score
+ }
 </script>
  
 <template>
-<h1 class="text-2xl mb-3 text-center">Vue from</h1>
-<div class="text-2xl mb-3 text-center ">
-   <p class="text-2xl mb-10 text-center ">{{ multibox }}</p>
-   <label >red</label>
-   <input type="checkbox" v-model="multibox" value="red" class="ml-2 mr-2" />
-   <label>green</label>
-   <input type="checkbox" v-model="multibox" value="green" class="ml-2 mr-2" />
-   <label>blue</label>
-   <input type="checkbox" v-model="multibox" value="blue" class="ml-2 mr-2" />
-</div>
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10 text-center">Vuejs Simple Quiz projects</h1>
+    <p class="mb-10">{{ saarsCountries }}</p>
+    <p class="mb-10 text-center">Total Score : {{ getscore() }}</p>
+    <div class="my-5 m-5 p-4 border border-gray-500" v-for="(country,index) in saarsCountries" :key="country.name" >
+      <p class="mb-1">Whats is the Capital of {{ country.name }} ?</p>
+      <template v-for="capital in getRandomCapitials()">
+      <input :name="country.name" type="radio" :value="capital"  v-model="country.answer">
+      <label for="" class="ml-2 mr-2">{{capital }}</label>
+      </template>
+    </div>
+  </section>
 </template>
  
 <style scoped>
