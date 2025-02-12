8e82a1a64c042757b609add05ad9c54c4a3590e2
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-12 10:50:39 +0600

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,21 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
- const newItem=ref(16)
-
-let chart = null
-const dataset=[
-    300,50,100
-]
-
- const data = {
-  labels: [
-    'Red',
-    'Blue',
-    'Yellow'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-};
-
-const config = {
-  type: 'pie',
-  data: data,
-};
-
-onMounted(()=>{
-    const ctx=document.getElementById('chart')
-    chart=new Chart(ctx,config)
-})
-
-function updateChart(){
-    dataset.push(newItem.value)
-    chart.data.datasets[0].data=dataset
-    chart.update()
-}
+ 
+  const multibox=ref([])
 </script>
  
 <template>
- <h1 class="m-5 text-center text-4xl">chart show | onmounted life hooks</h1>
-<div class="mx-auto w-[400px] h-[400px] bg-gray-400">
-    <canvas id="chart">
-    </canvas>
+<h1 class="text-2xl mb-3 text-center">Vue from</h1>
+<div class="text-2xl mb-3 text-center ">
+   <p class="text-2xl mb-10 text-center ">{{ multibox }}</p>
+   <label >red</label>
+   <input type="checkbox" v-model="multibox" value="red" class="ml-2 mr-2" />
+   <label>green</label>
+   <input type="checkbox" v-model="multibox" value="green" class="ml-2 mr-2" />
+   <label>blue</label>
+   <input type="checkbox" v-model="multibox" value="blue" class="ml-2 mr-2" />
 </div>
-<div class="mt-20 text-center">
-    <input type="text" v-model="newItem">
-    <button @click="updateChart()" class="ml-2 items-center bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Add 
-    </button>
-</div>
-
 </template>
  
 <style scoped>
