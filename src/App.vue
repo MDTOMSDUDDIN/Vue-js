edda4da9d37139c915fed0a3a8d2c74bccccad8a
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-24 16:26:25 +0600

    provide|inject

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <script setup>
-import { ref } from 'vue';
-import task from './components/task.vue';
-import Radio from './components/Radio.vue';
-const taskdone = ref(false);
+import { provide, reactive } from 'vue';
+import ComponentA from './components/componentA.vue';
+const person=reactive({
+  name:'tomas',
+  address:'sirajgonj',
+});
+provide('person',person); 
 
-const fruit = ref('apple');
 </script>
-
+ 
 <template>
-  <div class="p-12  m-auto">
-    Orginal Task:{{ taskdone }}
-    <br>
-    <div>
-      <task label="tihs is task" v-model="taskdone" />
-    </div>
-  </div>
-
-
-
-  <div class="text-center text-4xl">
-    <label>Your Favarite Fruits :{{ fruit }}</label>
-    <Radio v-model="fruit" label="apple" value="apple" />
-    <Radio v-model="fruit" label="orange" value="orange" />
-    <Radio v-model="fruit" label="banana" value="banana" />
-  </div>
+<ComponentA />
 </template>
-
-<style scoped></style>
+ 
+<style scoped>
+ 
+</style>
---
+++ src/components/componentA.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { inject } from 'vue';
+
+const person = inject('person'); 
+</script>
+
+<template>
+  <div>
+    <h2>Name: {{ person.name }}</h2>
+    <h3>Address: {{ person.address }}</h3>
+  </div>
+</template>
