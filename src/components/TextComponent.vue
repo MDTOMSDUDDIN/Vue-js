f4127465850c242d98c37cb58ccfdd3210a601f1
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-02-24 10:15:41 +0600

    component v-model

---
+++ src/components/TextComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps([
+   'label', 'modelValue',
+]);
+
+const emit = defineEmits('update:modelValue');
+</script>
+
+<template>
+   <div>
+      <p>
+         <label for="">{{ label }}</label><br>
+         <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+      </p>
+   </div>
+</template>
+
+<style scoped></style>
